b6d170980ac742a236c8fbdc9271c0e7983af065
Colin FitzGerald <colinfitzgerald328@gmail.com>
2023-05-06 23:48:10 -0700

    add favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
+import Head from 'next/head'
 import Root from '@/components'
 import { Inter } from 'next/font/google'
+import { Icon } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +11,12 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <Head>
+        <title>
+          Colin FitzGerald
+        </title>
+        <link rel="icon" href='https://colinfitzgerald328.github.io/assets/images/FitzGerald-Colin-Homework%206-01.jpg'></link>
+      </Head>
       <Root/>
     </main>
   )
